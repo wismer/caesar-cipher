5dd7ba5de049acf83a4c0d4cca9a3c3d4cafe861
wismer <matthewhl@gmail.com>
2015-09-18 11:22:03 -0400

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,34 @@ struct Cipher {
 }
 
 impl Cipher {
-    fn encode(self, message: &str) -> String {
+    fn process(&self, c: char, buffer: &mut String, shift: i32) {
+        if c.is_alphabetic() {
+            let corpus_loc = ALPHA.iter().position(|&a| a == c );
+            if corpus_loc.is_some() {
+                let actual = corpus_loc.unwrap() as i32;
+                let mut crypted_pos: usize;
+                if shift < 0 {
+                    if actual + shift > 0 {
+                        crypted_pos = ((actual + shift) as usize);
+                    } else {
+                        crypted_pos = (actual + shift + 25) as usize;
+                    }
+                } else {
+                    if actual + shift > 25 {
+                        crypted_pos = (actual + shift - 25) as usize;
+                    } else {
+                        crypted_pos = (actual + shift) as usize;
+                    }
+                }
+                let crypted = ALPHA[crypted_pos];
+                buffer.push(crypted);
+            }
+        } else {
+            buffer.push(c);
+        }
+    }
+
+    fn encode(&self, message: &str) -> String {
         let shift: i32 = match self.shift_dir {
             Direction::Right => self.shift_num as i32,
             Direction::Left => -(self.shift_num as i32),
@@ -20,31 +47,9 @@ impl Cipher {
         };
 
         let mut crypted_message = String::new();
+
         for c in message.chars() {
-            if c.is_alphabetic() {
-                let corpus_loc = ALPHA.iter().position(|&a| a == c );
-                if corpus_loc.is_some() {
-                    let actual = corpus_loc.unwrap() as i32;
-                    let mut crypted_pos: usize;
-                    if shift < 0 {
-                        if actual + shift > 0 {
-                            crypted_pos = ((actual + shift) as usize);
-                        } else {
-                            crypted_pos = (actual + shift + 25) as usize;
-                        }
-                    } else {
-                        if actual + shift > 25 {
-                            crypted_pos = (actual + shift - 25) as usize;
-                        } else {
-                            crypted_pos = (actual + shift) as usize;
-                        }
-                    }
-                    let crypted = ALPHA[crypted_pos];
-                    crypted_message.push(crypted);
-                }
-            } else {
-                crypted_message.push(c);
-            }
+            &self.process(c, &mut crypted_message, shift);
         }
 
         crypted_message
@@ -57,11 +62,13 @@ impl Cipher {
             Direction::Neither => 0i32
         };
 
+        let mut decoded_message = String::new();
+
         for c in encrypted_msg.chars() {
-            println!("{}", c);
+            &self.process(c, &mut decoded_message, shift);
         }
 
-        String::new()
+        decoded_message
     }
 }
 
@@ -84,5 +91,8 @@ fn main() {
         Starting development server at http://127.0.0.1:8000/
         Quit the server with CONTROL-C.");
 
-    println!("{}", encrypted);
+    {
+        let decrypted = reverse_cipher.decode(&encrypted);
+        println!("{}", decrypted);
+    }
 }
