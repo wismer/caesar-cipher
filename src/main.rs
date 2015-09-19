d64b6f9b6d684d2377c0616f028293eecc09e5a1
wismer <matthewhl@gmail.com>
2015-09-19 13:14:14 -0400

    light refactor

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,10 @@ impl Cipher {
             let corpus_loc = ALPHA.iter().position(|&a| a == c );
             if corpus_loc.is_some() {
                 let actual = corpus_loc.unwrap() as i32;
-                let mut crypted_pos: usize;
+                let crypted_pos: usize;
                 if shift < 0 {
                     if actual + shift >= 0 {
-                        crypted_pos = ((actual + shift) as usize);
+                        crypted_pos = (actual + shift) as usize;
                     } else {
                         crypted_pos = (actual + shift + 25) as usize;
                     }
@@ -74,23 +74,26 @@ impl Cipher {
 }
 
 fn main() {
+    let first_message = "that thing. did you get that thing i sent you.".to_string();
+    let second_message = "System check identified 1 issue (0 silenced).
+        September 16, 2015 - 21:27:47
+        Django version 1.8.4, using settings 'doc.settings.local'
+        Starting development server at http://127.0.0.1:8000/
+        Quit the server with CONTROL-C.".to_string();
+
     let cipher = Cipher {
         shift_num: 2,
         shift_dir: Direction::Right
     };
 
-    let encrypted = cipher.encode("that thing. did you get that thing i sent you.");
+    let encrypted = cipher.encode(&first_message);
 
     let reverse_cipher = Cipher {
         shift_num: 4,
         shift_dir: Direction::Left
     };
 
-    let encrypted_reverse = reverse_cipher.encode("System check identified 1 issue (0 silenced).
-        September 16, 2015 - 21:27:47
-        Django version 1.8.4, using settings 'doc.settings.local'
-        Starting development server at http://127.0.0.1:8000/
-        Quit the server with CONTROL-C.");
+    let encrypted_reverse = reverse_cipher.encode(&second_message);
 
     {
         let decrypted_reverse = reverse_cipher.decode(&encrypted_reverse);
